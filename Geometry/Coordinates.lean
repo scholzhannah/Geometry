176221a048963abdd7dc142680e50ac751b4980f
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2024-05-31 11:10:41 +0100

    timing-out co-ordinate version

---
+++ Geometry/Coordinates.lean
@@ -0,0 +1,43 @@
+import Mathlib
+
+noncomputable section
+
+open InnerProductGeometry
+open RealInnerProductSpace
+
+variable {V : Type*} [NormedAddCommGroup V] [InnerProductSpace ℝ V] {x y : V}
+
+/-- The undirected angle between two vectors. If either vector is 0,
+this is π/2. See `Orientation.oangle` for the corresponding oriented angle
+definition. -/
+theorem angle_eq_angle_of_squares (x y z w : V)
+    (h : ⟪x, y⟫^2 / (⟪x, x⟫ * ⟪y, y⟫) = ⟪z, w⟫^2 / (⟪z, z⟫ * ⟪w, w⟫)) :
+    angle x y = angle z w :=
+  sorry
+
+variable (x y z : EuclideanSpace ℝ (Fin 2))
+
+def perimeter : ℝ := dist y z  + dist z x + dist x y
+
+def incentre : EuclideanSpace ℝ (Fin 2) :=
+  (dist y z / perimeter x y z) • x + (dist z x / perimeter x y z) • y + (dist x y / perimeter x y z) • z
+
+set_option maxHeartbeats 5000000
+example : angle (incentre x y z - x) (y - x) = angle (incentre x y z - x) (z - x) := by
+  apply angle_eq_angle_of_squares
+  have : (y 0 - x 0) * (y 0 - x 0) + (y 1 - x 1) * (y 1 - x 1) ≠ 0 := sorry
+  have : (dist y z * x 0 + dist z x * y 0 + dist x y * z 0 - (dist y z + dist z x + dist x y) * x 0) *
+            (dist y z * x 0 + dist z x * y 0 + dist x y * z 0 - (dist y z + dist z x + dist x y) * x 0) +
+          (dist y z * x 1 + dist z x * y 1 + dist x y * z 1 - (dist y z + dist z x + dist x y) * x 1) *
+            (dist y z * x 1 + dist z x * y 1 + dist x y * z 1 - (dist y z + dist z x + dist x y) * x 1) ≠ 0 := sorry
+  have : (z 0 - x 0) * (z 0 - x 0) + (z 1 - x 1) * (z 1 - x 1) ≠ 0 := sorry
+  have : (dist y z * x 0 + dist z x * y 0 + dist x y * z 0 - (dist y z + dist z x + dist x y) * x 0) *
+            (dist y z * x 0 + dist z x * y 0 + dist x y * z 0 - (dist y z + dist z x + dist x y) * x 0) +
+          (dist y z * x 1 + dist z x * y 1 + dist x y * z 1 - (dist y z + dist z x + dist x y) * x 1) *
+            (dist y z * x 1 + dist z x * y 1 + dist x y * z 1 - (dist y z + dist z x + dist x y) * x 1) ≠ 0 := sorry
+  have : perimeter x y z ≠ 0 := sorry
+  dsimp [incentre, perimeter, angle, -ne_eq] at *
+  simp only [map_sub, map_add, map_mul, map_div₀, conj_trivial, Fin.sum_univ_two, dist_comm] at *
+  field_simp
+  -- ring
+  sorry
